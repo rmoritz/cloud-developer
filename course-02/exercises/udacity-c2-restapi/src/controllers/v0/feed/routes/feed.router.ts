c34fe93c27bb71e071cc7e58eed733d7597bbfb2
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2020-02-24 04:23:57 +0100

    Fix build target in package.json, fix file modes

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -16,17 +16,49 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
-//@TODO
-//Add an endpoint to GET a specific resource by Primary Key
+// Get a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    if (!item) {
+        res.status(404).send("no such item");
+    }
+    else {
+        item.url = AWS.getGetSignedUrl(item.url);
+        res.send(item);
+    }
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
-});
+        let { id } = req.params;
 
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+        
+        // check either caption or url have been provided
+        if (!caption && !fileName) {
+            return res.status(400).send({ message: 'One of either caption or file url is required' });
+        }
+
+        // lookup item
+        const item = await FeedItem.findByPk(id);
+        if (!item) {
+            return res.status(404).send("no such item");
+        }
+
+        // update item
+        item.caption = caption || item.caption;
+        item.url = fileName || item.url;
+
+        // save item & set signed url
+        const saved_item = await item.save();
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        
+        res.status(200).send(saved_item);
+});
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName', 
